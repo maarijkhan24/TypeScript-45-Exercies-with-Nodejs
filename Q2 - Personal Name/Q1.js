16330f30a1c0b0637cd88a705d2b6a2f927a1794
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-04-17 22:55:36 +0500

    Add files via upload

---
+++ Q2 - Personal Name/Q1.js
@@ -0,0 +1,4 @@
+//let personalName =  "Maarij";
+//console.log("Hello", personalName, "would you like to learn some Python today?");
+var personalName = "maarij";
+console.log("Hello", personalName, "would you like to learn some Python today?");
---
+++ Q2 - Personal Name/Q1.ts
@@ -0,0 +1,5 @@
+//let personalName =  "Maarij";
+//console.log("Hello", personalName, "would you like to learn some Python today?");
+
+let personalName = "maarij";
+console.log("Hello", personalName,"would you like to learn some Python today?");
